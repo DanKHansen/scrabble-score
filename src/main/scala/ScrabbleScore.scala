8169e602e23cb7ac6a97098feca8a6f6fad7563d
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-16 10:47:24 +0200

    Initial commit

---
+++ src/main/scala/ScrabbleScore.scala
@@ -0,0 +1,11 @@
+object ScrabbleScore:
+   def score(s: String): Int =
+      s.toUpperCase.toSeq.map {
+         case 'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1
+         case 'D' | 'G'                                                 => 2
+         case 'B' | 'C' | 'M' | 'P'                                     => 3
+         case 'F' | 'H' | 'V' | 'W' | 'Y'                               => 4
+         case 'K'                                                       => 5
+         case 'J' | 'X'                                                 => 8
+         case 'Q' | 'Z'                                                 => 10
+      }.sum
